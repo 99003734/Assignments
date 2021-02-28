6d10ac8e84a36511bdc082e8bfe9180b17bc2a62
99003734 <78858575+99003734@users.noreply.github.com>
2021-03-01 00:22:27 +0530

    Create Makefile.c

---
+++ Assignment-1/Makefile.c
@@ -0,0 +1,10 @@
+build:
+	gcc systemcalls-1.c -o sys_one
+	gcc systemcalls-2.c -o two
+	gcc systemcalls-3.c -o three
+	gcc process-1.c -o four
+  gcc process-2.c -o five
+  gcc process-3.c -o six
+	gcc threads-1.c -o seven
+	gcc threads-2.c -o eight
+  gcc threads-3.c -o nine
