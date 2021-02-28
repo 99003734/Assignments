15923cb127b02982b728654030b9d32af3035529
99003734 <78858575+99003734@users.noreply.github.com>
2021-03-01 01:02:51 +0530

    Update Makefile.c

--- Assignment-2/Makefile.c
+++ Assignment-2/Makefile.c
@@ -1,10 +1,16 @@
 build:
-	gcc Client_string_4.c -o one
-	gcc Server_string.c_4 -o two
-	gcc stack.c_1 -o stack
-	gcc Stack_mutex.c_3 -o mutex
-	gcc sender2_5.c -o send2
-	gcc rec2_5.c -o rec2
+	gcc stack_Operatoins(1).c -o stack
+	gcc circular_Buffer_Oper(2).c_4 -o two
+	gcc mutex_Operations(3).c_1 -o mutex
+	gcc client_Msg_Queues(4).c_3 -o send2
+	gcc server_Msg_Queues(4).c -o 
+	gcc client_Msg_Queues(6).c_3 -o send2
+	gcc server_Msg_Queues(6).c -o 
+	gcc readfile(7).c
+	gcc write_file(7).c
+	gcc producer(8).c
+	gcc consumer(8).c
+	
 
 
 
